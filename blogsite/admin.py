f8267566fe19de4e72e7625745a45bc49283d50a
Brian Martin <brianm@infinityvip.com>
2018-03-28 16:37:44 -0500

    Removing unused files

--- blogsite/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from blogsite.models import Post
-from blogsite.models import Comment
-
-admin.site.register(Post)
-admin.site.register(Comment)
--- blogsite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('$/', views.post, name='posts'),
-	path('$/', views.comments, name='comments'),
-]
