0ccba9e8ae56cebe9cfc6fe4d27045be58766d20
Brian Martin <brianm@infinityvip.com>
2018-03-28 21:34:46 -0500

    A few last changes

--- articles/views.py
+++ articles/views.py
@@ -6,7 +6,7 @@ from . import forms
 
 # Grab all blog posts and authors
 def article_list(request):
-	articles = Article.objects.all().order_by('date')
+	articles = Article.objects.all().order_by('-date')
 	return render(request, 'articles/article_list.html', { 'articles': articles })
 
 # View individual blog article
